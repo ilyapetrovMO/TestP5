1ff6598b0f7c1d17dfefdf5ed95067a711516421
Ilya Petrov <ilyapetrov.mosobl@gmail.com>
2018-12-17 19:28:32 +0300

    noice progress

--- Testing/sketch.js
+++ Testing/sketch.js
@@ -1,32 +1,64 @@
+
+
 function preload(){
-  tree = loadModel('lowpolytree.obj', normalize);
+ dolphin      = loadModel('13182_LeapingDolphinStatue_v1_NEW.obj', true);
+ thinker      = loadModel('the_thinker.obj', true);
+ blankTexture = loadImage('blank_texture.jpg');
+ gridTexture  = loadImage('grid.jpg');
+ palmTree     = loadModel('MY_PALM.obj');
 }
 
 function setup() {
   // put setup code here
-  createCanvas(640, 480, WEBGL);
-  background(100);
-  frameRate(60);
+  createCanvas(1200, 700, WEBGL);
+  frameRate(5);
 }
 function draw() {
-  background(100);
-  var x1 = map(mouseX, 0 , width, 0, 5);
-  var y1 = map(mouseY, 0 , width, 0, 5);
-  rotateY(x1);
-  rotateX(y1);
-  model(tree);
-  
-  // put drawing code here
-  // background(100);
-  // strokeWeight(1);
-  // ellipse(width/2, height/2, 300);
-  // xPoint = width/2 + (150 * cos(angle));
-  // yPoint = height/2 + (150 * sin(angle));
-  // stroke(200);
-  // strokeWeight(1);
-  // line(width/2, height/2, xPoint, yPoint);
-  // stroke(51);
-  // strokeWeight(5);
-  // point(xPoint, yPoint);
-  // angle = angle + 0.1;
+directionalLight(200, 20, 200, -200);
+directionalLight(220, 0, 100, -5000);
+background(22);
+cube();
+earth();
+palm();
 }
+
+function cube(){
+  push();
+  ambientMaterial(222, 200, 210);
+  rotateY(radians(180));
+  rotateZ(radians(10));
+  xR = map(mouseX, 0, width, 0, 1);
+  yR = map(mouseY, height, 0, 0, 1);
+  rotateX(yR);
+  rotateY(xR);
+  texture(blankTexture);
+  scale(2);
+  model(thinker);
+  //box(200, 200, 200);
+  pop();
+}
+
+function earth(){
+  push();
+  texture(gridTexture);
+  translate(0, 300);
+  rotateX(radians(90));
+  plane(5000, 5000);
+  pop();
+}
+
+function palm() {
+  push();
+  rotateX(radians(180));
+  translate(400, -600, 200);
+  model(palmTree);
+  pop();
+
+  push();
+  rotateX(radians(180));
+  rotateY(radians(180));
+  translate(400,-600,-200);
+  model(palmTree);
+  pop();
+  }
+  
